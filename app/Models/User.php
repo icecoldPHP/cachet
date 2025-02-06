a4636debb5166f3465d717a5da6a72bb2d51ca8d
Sven <sven@vioms.com>
2025-02-06 22:46:18 +0100

    Remove duplicate data

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,43 +24,6 @@ class User extends CachetUser implements FilamentUser
     use HasApiTokens;
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'is_admin',
-        'preferred_locale',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password'          => 'hashed',
-            'is_admin'          => 'boolean',
-        ];
-    }
-
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
