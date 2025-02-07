3750a5ad55d1667446368f44ca18d0c03a6f4c96
Sven <sven@vioms.com>
2025-02-07 03:08:49 +0100

    Add schedules to timeline

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,44 @@ class User extends CachetUser implements FilamentUser
     use HasApiTokens;
     use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'is_admin',
+        'preferred_locale',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'is_admin' => 'bool',
+        ];
+    }
+
+
     public function canAccessPanel(Panel $panel): bool
     {
         return true;
